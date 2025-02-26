d7ee7fe2cb17e5fe07a061c38273ea39a0d74534
/cris <117968518+AIcris@users.noreply.github.com>
2025-02-26 22:59:15 +0800

    Add files via upload

---
+++ sminventorydb.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sminventorydb
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `sminventorydb`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sminventorydb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `sminventorydb`;
+
+--
+-- Table structure for table `tbl_account`
+--
+
+DROP TABLE IF EXISTS `tbl_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_account` (
+  `accID` int NOT NULL AUTO_INCREMENT,
+  `accUser` varchar(30) DEFAULT NULL,
+  `accPass` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`accID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_account`
+--
+
+LOCK TABLES `tbl_account` WRITE;
+/*!40000 ALTER TABLE `tbl_account` DISABLE KEYS */;
+INSERT INTO `tbl_account` VALUES (1,'admin','root');
+/*!40000 ALTER TABLE `tbl_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_inventory`
+--
+
+DROP TABLE IF EXISTS `tbl_inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_inventory` (
+  `invID` varchar(30) NOT NULL,
+  `invName` varchar(100) DEFAULT NULL,
+  `invType` varchar(30) DEFAULT NULL,
+  `invPrice` float DEFAULT NULL,
+  `invStock` int DEFAULT NULL,
+  `invStatus` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`invID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_inventory`
+--
+
+LOCK TABLES `tbl_inventory` WRITE;
+/*!40000 ALTER TABLE `tbl_inventory` DISABLE KEYS */;
+INSERT INTO `tbl_inventory` VALUES ('SM145','Jollibee OG Fried Chicken 1kg','Food',250,1000,'OverStocked'),('SM181','Kikkoman Soy Sauce 500mL','Condiment',150,450,'Normal'),('SM210','Kenwood Deep Fryer (Cap. 1kg)','Equipment',25000,50,'Understock');
+/*!40000 ALTER TABLE `tbl_inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-26 22:54:32
